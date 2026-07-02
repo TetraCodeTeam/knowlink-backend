2ec2fe2d68c01a43d4e94b28c709064f1ff024d0
mroja <mrojaslopez03@gmail.com>
2026-07-02 02:25:35 -0300

    feat: agrega el modelo Token y actualiza los roles de la app.

---
+++ src/main/java/com/knowlink/api/users/data/models/Token.java
@@ -0,0 +1,42 @@
+package com.knowlink.api.users.data.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tokens")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString(onlyExplicitlyIncluded = true)
+public class Token {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "token_id", updatable = false, nullable = false)
+    @EqualsAndHashCode.Include
+    @ToString.Include
+    private UUID tokenId;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "token_expiration_date", nullable = false)
+    private LocalDateTime tokenExpirationDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        tokenExpirationDate = LocalDateTime.now().plusHours(12);
+    }
+}
