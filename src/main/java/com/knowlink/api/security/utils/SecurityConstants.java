97a88214f237381a42827939a5673b73471e4d8c
mroja <mrojaslopez03@gmail.com>
2026-07-08 18:55:53 -0300

    feat: agrega nuevas rutas a la lista whitelist en SecurityConstants #5 #15

--- src/main/java/com/knowlink/api/security/utils/SecurityConstants.java
+++ src/main/java/com/knowlink/api/security/utils/SecurityConstants.java
@@ -2,23 +2,28 @@ package com.knowlink.api.security.utils;
 
 public class SecurityConstants {
 
-    private SecurityConstants() {}
+        private SecurityConstants() {
+        }
 
-    public static final String[] PUBLIC_WHITELIST = {
-            "/api/v1/auth/login",
-            "/api/v1/auth/register",
-            "/api/v1/users/*/verify-account",
-            "/api/v1/users/resend-verification-account",
-            "/api/v1/users/reset-password/**",
-            "/api/v1/public/**",
-            "/actuator/health",
-    };
+        public static final String[] PUBLIC_WHITELIST = {
+                "/api/v1/auth/login",
+                "/api/v1/auth/register",
+                "/api/v1/auth/register/tutor",
+                "/api/v1/users/*/verify-account",
+                "/api/v1/users/resend-verification-account",
+                "/api/v1/users/reset-password/**",
+                "/api/v1/public/**",
+                "/actuator/health",
+                "/api/v1/careers",
+                "/api/v1/subjects",
+                "/api/v1/subjects/basic",
+        };
 
-    public static final String[] SWAGGER_WHITELIST = {
-            "/api-docs",
-            "/api-docs/**",
-            "/swagger-ui.html",
-            "/swagger-ui/**",
-            "/v3/api-docs/**",
-    };
+        public static final String[] SWAGGER_WHITELIST = {
+                "/api-docs",
+                "/api-docs/**",
+                "/swagger-ui.html",
+                "/swagger-ui/**",
+                "/v3/api-docs/**",
+        };
 }
